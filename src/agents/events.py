203090b77e6a14b5d7b440fca9ca4c96e8df54d8
TheGh0xt <adetoyeanointing15@gmail.com>
2026-06-30 14:38:28 +0100

    fix(import error): use conventional importation to fix adk error and refactor a bit of prompts

--- src/agents/events.py
+++ src/agents/events.py
@@ -1,21 +1,23 @@
 import os
 
 from google.adk.agents.llm_agent import LlmAgent
-from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
+from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
 
-from src.prompts.events import SYSTEM_PROMPT
+from ..prompts.events import SYSTEM_PROMPT
 
 market_event_agent = LlmAgent(
     model="gemini-2.5-flash",
     name="market_event_agent",
     description="You are the Event Retrieval Agent for the Polymarket Intelligence Engine.",
     instruction=SYSTEM_PROMPT,
-    mode="task",
+    # mode="task",
     tools=[
-        MCPToolset(
-            connection_params=os.getenv(
-                "SAGITTARIUS_MCP_URL", "http://localhost:8080/mcp"
-            )
+        McpToolset(
+            connection_params=StreamableHTTPConnectionParams(
+                url=os.getenv("SAGITTARIUS_MCP_URL", "http://localhost:8080/mcp")
+            ),
+            # tool_filter=["get_event_by_slug"],
         ),
     ],
     output_key="event_details_output",
